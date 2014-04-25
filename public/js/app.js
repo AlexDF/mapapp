466cd22b20add1cb74bdd94121398569a8e157f8
Alexandre DeFreitas <abd9@njit.edu>
2014-04-25 16:13:35 -0400

    got map to show up in browser through backbone

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,34 @@
 var AppRouter = Backbone.Router.extend({
   routes: {
-    "": "nearbySchools"
+    "": "nearbySchools",
+    "map": "showMap"
   },
 
-  /*initialize: function() {
+  initialize: function() {
+    
+    var geocoder = new google.maps.Geocoder();
+  },
 
-  },*/
+  showMap: function() {
+    var mapOptions = {
+      zoom: 14, 
+      center: new google.maps.LatLng(-60, 150)
+    };
+    var mapDiv = document.getElementById('content');
+    var map = new google.maps.Map(mapDiv, mapOptions);
+  },
 
   nearbySchools: function() {
-    $('#content').html('backbone');
+    //$('#content').html(this.myvar);
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        //var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        $('#content').html(position.coords.latitude);
+        
+      });
+    } else {
+      // Browser doesn't support Geolocation
+    }
   }
 
 
