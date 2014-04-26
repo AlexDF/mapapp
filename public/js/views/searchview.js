becfa7b66e3295c19913487ea0a263ac759e9d45
Alexandre DeFreitas <abd9@njit.edu>
2014-04-26 03:29:35 -0400

    able to get search feature to query mongodb database and show result in browser

--- public/js/views/searchview.js
+++ public/js/views/searchview.js
@@ -2,7 +2,7 @@ var SearchView = Backbone.View.extend({
 
   render: function() {
     var markup = '<div>' + 
-      '<form method="get" action="/findschool">' +
+      '<form method="post" action="/findschool">' +
         '<img src="../../img/glass.jpg"><label for="school_name">Search</label>' + '<br>' +
         '<input type="text" name="school_name" placeholder="Enter a School Name">' +
         '<input type="submit" value="Search">' + '</form>' + 
--- server.js
+++ server.js
@@ -1,6 +1,42 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var fs = require('fs');
+var csv = require('csv');
+var records = new Array();
+var output = "";
+var bodyParser = require('body-parser');
 
-var app = express().use(express.static('public'));
+var app = express();
+app.use(express.static('public'));
+app.use(bodyParser());
+
+
+mongoose.connect('mongodb://localhost/directory/');
+var db = mongoose.connection;
+
+var schoolSchema = mongoose.Schema({
+      countyName: String,
+      schoolName: String,
+      address: String,
+      city: String,
+      state: String,
+      zip: String,
+      phone: String
+    });
+
+
+var School = mongoose.model('School', schoolSchema);
+
+
+app.post('/findschool', function(req, res) {
+    School.find({ schoolName: req.body.school_name }, function(err, school) {
+      if(err) {console.log('Error: ' + err);}
+      else {
+        var match = school[0];
+        res.send('<a href="/#/">Return</a>');
+      }
+    }); // end School.find()
+}); // end app.post('/findschool',)
 
 
 app.get('/hello', function(req, res) {
